1878e0666ed458040a6261fe8e100e798d954e4f
melih <nmlhky@gmail.com>
2020-02-03 20:27:43 -0600

    3rd day

---
+++ src/ThirdDay/ifelse.java
@@ -0,0 +1,14 @@
+package ThirdDay;
+
+public class ifelse {
+    public static void main(String[] args) {
+        char a = 'a';
+        char b = 'a';
+
+        if (a>b) {
+            System.out.println("aaa");
+        }else {
+            System.out.println("bbb");
+        }
+    }
+}
