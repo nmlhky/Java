04ba50cb03e555053df7f08e839ea89a0a78b397
melih <nmlhky@gmail.com>
2020-02-03 20:44:44 -0600

    3rd day

---
+++ src/ThirdDay/odd.java
@@ -0,0 +1,14 @@
+package ThirdDay;
+
+import java.util.Scanner;
+
+public class odd {
+    public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+        int num = number.nextInt();
+
+        if(num%2 == 1){
+            System.out.println("odd");
+        }
+    }
+}
