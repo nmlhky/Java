f601d7e2b4400dc0509972160f13a1fd3395de6c
melih <nmlhky@gmail.com>
2020-02-04 19:54:07 -0600

    homework

---
+++ src/Homework/store.java
@@ -0,0 +1,12 @@
+package Homework;
+import java.util.Scanner;
+public class store {
+    public static void main(String[] args) {
+        String[] ItemName = {"Apple", "Pear", "pineapple", "Watermelon", "Orange" };
+        float[] Item = {2.5, 20, 30, 40};
+
+
+
+
+    }
+}
