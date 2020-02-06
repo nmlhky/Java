40b94764f53bd1afe3db28ad26b58318418c61a4
melih <nmlhky@gmail.com>
2020-02-05 20:04:25 -0600

    4th day

---
+++ src/FourthDay/jacket.java
@@ -0,0 +1,14 @@
+package FourthDay;
+import java.util.Scanner;
+public class jacket {
+    public static void main(String[] args) {
+        Scanner Tempature = new Scanner(System.in);
+        int tem = Tempature.nextInt();
+
+        if (tem<=30){
+            System.out.println("wear jacket");
+        }else{
+            System.out.println("wear hat");
+        }
+    }
+}
---
+++ src/FourthDay/triangle.java
@@ -0,0 +1,18 @@
+package FourthDay;
+import java.util.Scanner;
+public class triangle {
+    public static void main(String[] args) {
+        Scanner edges = new Scanner(System.in);
+        int a = edges.nextInt();
+        int b = edges.nextInt();
+        int c = edges.nextInt();
+
+        if (a+b>c & a+c>b & b+c>a){
+            System.out.println("okay");
+        }
+        else {
+            System.out.println("not okay");
+        }
+
+    }
+}
