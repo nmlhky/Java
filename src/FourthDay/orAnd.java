b2f1c046e6a93fd033f09ff6c8933e682296e863
melih <nmlhky@gmail.com>
2020-02-05 20:27:32 -0600

    4th day

---
+++ src/FourthDay/orAnd.java
@@ -0,0 +1,9 @@
+package FourthDay;
+
+public class orAnd {
+    public static void main(String[] args) {
+        if (12>20 || 20 < 30 && 1<24){
+            System.out.println("yes");
+        }
+    }
+}
