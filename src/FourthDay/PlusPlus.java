fcf2b824b2ad52edc62ad5b3e058ba398e83b17c
melih <nmlhky@gmail.com>
2020-02-05 18:17:30 -0600

    4th day

---
+++ src/FourthDay/PlusPlus.java
@@ -0,0 +1,13 @@
+package FourthDay;
+
+public class PlusPlus {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = ++x;
+        System.out.println(""+x+y);
+
+        int a = 5;
+        int b = x++;
+        System.out.println(""+a+b);
+    }
+}
