32ffc21d01ebac98fbacf340d549bdbe0f278bb9
melih <nmlhky@gmail.com>
2020-02-08 13:51:09 -0600

    Feb 8

---
+++ src/Day5/Gender.java
@@ -0,0 +1,16 @@
+package Day5;
+import java.util.Scanner;
+public class Gender {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        char gender = scan.next().charAt(0);
+
+        if(gender == 'm'){
+            System.out.println("Male");
+        }else if(gender == 'f'){
+            System.out.println("Female");
+        }else{
+            System.out.println("Wrong answer enter m or f");
+        }
+    }
+}
