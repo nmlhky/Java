47bdf19d138012c6feab77f2b71eb94a6d809ef8
melih <nmlhky@gmail.com>
2020-02-09 14:32:20 -0600

    Feb 9

---
+++ src/Day6/Bill.java
@@ -0,0 +1,22 @@
+package Day6;
+import java.util.Scanner;
+public class Bill {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("are you member y or n :");
+        String member = scanner.nextLine();
+
+        if (member.equals("y")){
+            System.out.println("how many gb data do you need : ");
+            int data = scanner.nextInt();
+
+            if (data <101){
+                System.out.println("$10");
+            } else {
+                System.out.println("$40");
+            }
+        } else {
+            System.out.println("you need a membership");
+        }
+    }
+}
---
+++ src/Day6/SwitchOparator.java
@@ -0,0 +1,33 @@
+package Day6;
+
+public class SwitchOparator {
+    public static void main(String[] args) {
+
+        int grade = 4;
+
+        switch (grade) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+
+    }
+}
