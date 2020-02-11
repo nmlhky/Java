a0637a9a776b340ceee8fb3a88381389c5d20a45
melih <nmlhky@gmail.com>
2020-02-10 20:53:26 -0600

    Feb 9

--- src/day7/forLoop.java
+++ src/day7/forLoop.java
@@ -1,10 +1,19 @@
 package day7;
-
+import java.util.Scanner;
 public class forLoop {
     public static void main(String[] args) {
-        for (int i = 1; i < 101 ; i++) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int num2 = 0;
+
+        for ( ; num < num2 ; ) {
+            num2 = scanner.nextInt();
+        }
+
+        for (int i = num; i < num2+1 ; i++) {
+            if (i%5==0) {
                 System.out.println(i);
-                i++;
+            }
         }
     }
 }
---
+++ src/day7/forLoop2.java
@@ -0,0 +1,12 @@
+package day7;
+
+public class forLoop2 {
+    public static void main(String[] args) {
+        for (int i = 100 ; i >= 0 ; i -= 2 ){
+            System.out.println(i);
+        }
+        for (int i = 2000 ; i >= 1000 ; i -= 2 ){
+            System.out.println(i);
+        }
+    }
+}
