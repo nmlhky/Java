7623f08931bd149419009313b2c0c206fcb870d6
melih <nmlhky@gmail.com>
2020-02-10 19:54:58 -0600

    Feb 9

---
+++ src/day7/forLoop.java
@@ -0,0 +1,10 @@
+package day7;
+
+public class forLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i < 101 ; i++) {
+                System.out.println(i);
+                i++;
+        }
+    }
+}
