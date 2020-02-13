0e60710ae4865a8db4d67b82fd8de3fa9000193e
melih <nmlhky@gmail.com>
2020-02-13 14:58:19 -0600

    Feb 13

---
+++ src/hackerRank/Flag.java
@@ -0,0 +1,35 @@
+package hackerRank;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Flag {
+
+        static boolean flag = true;
+        static int B,H;
+
+        static{ Scanner sc = new Scanner(System.in);
+
+        B = sc.nextInt();
+        H = sc.nextInt();
+        boolean flag = true;
+        if(B<=0||H<=0){
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+            flag = false;
+            System.exit(0);
+        }
+
+        }
+
+        public static void main(String[] args){
+            if(flag){
+                int area=B*H;
+                System.out.print(area);
+            }
+
+        }//end of main
+
+    }//end of class
+
