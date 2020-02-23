4386fbd0490b33c75c5aad114d29cbf52732bde3
melih <nmlhky@gmail.com>
2020-02-22 19:31:02 -0500

    Feb 22

---
+++ src/day12/NumberPower.java
@@ -0,0 +1,12 @@
+package day12;
+import java.math.*;
+import java.util.*;
+public class NumberPower {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a= scan.nextInt();
+        int b= scan.nextInt();
+
+        System.out.println(Math.pow(a,b));
+    }
+}
---
+++ src/day12/RootNumber.java
@@ -0,0 +1,8 @@
+package day12;
+
+public class RootNumber {
+    public static void main(String[] args) {
+        int a = 16;
+        System.out.println(Math.sqrt(a));
+    }
+}
