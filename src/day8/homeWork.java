9976f8edf001418ee135e722db16646feec080d0
melih <nmlhky@gmail.com>
2020-02-29 17:04:19 -0600

    Feb 29

--- src/day8/homeWork.java
+++ src/day8/homeWork.java
@@ -7,14 +7,17 @@ public class homeWork {
         for (int n = 1; n < 11; n++){
             System.out.print("\t"+n);
         }
-        System.out.println("\n-----------------------------------------------");
+        System.out.println("\n------------------------------------------------");
         for (int i = 1; i < 11; i++) {
             System.out.print(i+"\t|");
             for (int j = 1; j < 11; j++) {
                 System.out.print("\t"+i*j);
             }
+            System.out.print("\t|");
             System.out.println("");
         }
+
+        System.out.println("-------------------------------------------------");
 
     }
 }
