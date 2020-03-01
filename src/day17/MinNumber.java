6b1fe31c8d4f4991f0b343a636df4056e3517d70
melih <nmlhky@gmail.com>
2020-03-01 15:42:14 -0600

    march 1

---
+++ src/day17/MinNumber.java
@@ -0,0 +1,14 @@
+package day17;
+
+public class MinNumber {
+    public static void main(String[] args) {
+        int[] a ={1,2,3,434,5,45,435,435,-2};
+        int min = a[0];
+        for (int i : a){
+            if (i<min){
+                min=i;
+            }
+        }
+        System.out.println(min);
+    }
+}
