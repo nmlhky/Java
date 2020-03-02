17d4345ea63112d7d2d586ab3ea1e829cdc91d37
melih <nmlhky@gmail.com>
2020-03-02 14:39:13 -0600

    march 2

--- src/hackerRank/anagram.java
+++ src/hackerRank/anagram.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 public class anagram {
     static boolean isAnagram(String a, String b) {
         // Complete the function
-        boolean isAnagrams = true;
-        int x= a.length();
-        int y = b.length()-1;
-        for(int i = 0 ; i < x ; i++){
-            if(a.charAt(i) != b.charAt(y-i)){
-                isAnagrams = false;
-            }
+        if(a.length() != b.length()) return false;
+        int c[] = new int[26], d[] = new int[26] ;
+        a = a.toUpperCase();
+        b = b.toUpperCase();
+        for(int i=0; i<a.length(); i++){
+            c[a.charAt(i) - 'A']++;
+            d[b.charAt(i) - 'A']++;
         }
-        return isAnagrams;
+
+        for(int i =0;i<26; i++) {
+            //System.out.println(c[i]);
+            if (c[i] != d[i]) return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
