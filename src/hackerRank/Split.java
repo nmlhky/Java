64a5097e909c89f084bb0f1e53edffb0383c98ad
melih <nmlhky@gmail.com>
2020-03-02 21:03:29 -0600

    march 2

---
+++ src/hackerRank/Split.java
@@ -0,0 +1,31 @@
+package hackerRank;
+
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Split {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        String[] items = s.trim().split("[ !,?.\\_'@]+");
+
+        if(s == ""|| s.trim().equals("")){
+            System.out.println("0");
+        }
+        else if(s.length() > 400000){
+            return;
+        }
+        else{
+            System.out.println(items.length);
+        }
+
+        for(String item: items){
+            System.out.println(item);
+        }
+
+
+
+        scan.close();
+    }
+}
