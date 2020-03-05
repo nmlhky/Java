3fd560842986b7ce136231e291a7eca21df56b1f
melih <nmlhky@gmail.com>
2020-03-04 18:03:44 -0600

    march 4

---
+++ src/LearnNewThing/TryCatch.java
@@ -0,0 +1,12 @@
+package LearnNewThing;
+
+public class TryCatch {
+    public static void main(String[] args) {
+        try {
+            int[] myNumbers = {1, 2, 3};
+            System.out.println(myNumbers[10]);
+        } catch (Exception e) {
+            System.out.println("Something went wrong.");
+        }
+    }
+}
