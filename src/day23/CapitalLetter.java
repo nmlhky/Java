702fa66453615c9d5d5419f590da2ad50168a214
melih <nmlhky@gmail.com>
2020-03-12 20:59:17 -0500

    march 12

--- src/day23/CapitalLetter.java
+++ src/day23/CapitalLetter.java
@@ -2,11 +2,11 @@ package day23;
 
 public class CapitalLetter {
     public static void main(String[] args) {
-
+        String a = "asd";
+        capitalLetterNumber(a);
     }
 
-    public static int capitalLetterNumber(String sentence){
-
+    public static void capitalLetterNumber(String sentence){
         int count = 0 ;
 
         for (int i = 0 ; i<sentence.length() ; i++){
@@ -14,6 +14,11 @@ public class CapitalLetter {
                 count++;
         }
 
-        return count;
+        if (count > 4){
+            System.out.println("Accepts");
+        } else {
+            System.out.println("Reject");
+        }
+
     }
 }
