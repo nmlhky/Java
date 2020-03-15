d3dfc48c2d6e9d643509c22376c39037099ed6ae
melih <nmlhky@gmail.com>
2020-03-15 17:02:02 -0500

    march 15

---
+++ src/day26/DesignAShapesTest.java
@@ -0,0 +1,12 @@
+package day26;
+
+public class DesignAShapesTest {
+    public static void main(String[] args) {
+        DesignAShapes star = new DesignAShapes('*',25,15);
+        star.printPyramid();
+
+        System.out.println("-------------");
+
+        star.printRectangle();
+    }
+}
