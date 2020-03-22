16b98a1db195e59468936e7719a5ab6137a34b7d
melih <nmlhky@gmail.com>
2020-03-22 17:33:21 -0500

    march 22

---
+++ src/day31/A.java
@@ -0,0 +1,12 @@
+package day31;
+
+public class A {
+    int a;
+    int b;
+
+    public A(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+}
---
+++ src/day31/Bank.java
@@ -0,0 +1,34 @@
+package day31;
+
+public class Bank {
+    int accountNumber;
+    String accountName;
+    Double balance;
+
+    public Bank(int accountNumber, String accountName, Double balance) {
+        this.accountNumber = accountNumber;
+        this.accountName = accountName;
+        this.balance = balance;
+    }
+
+    void AddMoney(){
+        this.balance++;
+    }
+
+    void DrawMoney(){
+        this.balance--;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    @Override
+    public String toString() {
+        return "Bank{" +
+                "accountNumber=" + accountNumber +
+                ", accountName='" + accountName + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
