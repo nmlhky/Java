e543c054cd47dc3c239b8be17bf3183c35fc3d87
melih <nmlhky@gmail.com>
2020-04-18 02:35:02 -0400

    Commit Message

--- src/Test.java
+++ src/Test.java
@@ -1,30 +1,85 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        int[] c = new  int[] {0,0,0,0,1,0};
 
-        System.out.println(jumpingOnClouds(c));
+        System.out.println(Arrays.toString(removeDups(new  int[] {0,0,2,2,1,0})));
+
+        System.out.println(capLast("java is super fun"));
+
+        System.out.println(sumTwoSmallestNums2(new int[]{19, 5, -2, 42, 2, 77}));
 
     }
 
 
-    static int jumpingOnClouds(int[] c) {
-        int count = 0 ;
-        int n = c.length;
-        for (int i = 0; i < n-2 ; count++) {
-            if (c[i+2]==0  || c[i] == 1 ) i = i+2;
-            else i++;
+    public static int[] removeDups(int[] nums) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int n : nums) {
+            if (!list.contains(n)) list.add(n);
         }
-        if (c[n-2]==0) count++;
-        return count;
 
+        int[] result = new int[list.size()];
 
-
-        int count = -1;
-        for (int i = 0; i < n; i++, count++) {
-            if (i<n-2 && c[i+2]==0) i++;
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
         }
-        System.out.println(count);
+
+        return result;
+    }
+
+    public static String[] removeDups(String[] str) {
+        ArrayList<String> list = new ArrayList<>();
+
+        for (String s : str) {
+            if (!list.contains(s)) list.add(s);
+        }
+
+        String[] result = new String[list.size()];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+
+    public static String capLast(String str) {
+        String s = "";
+        for (int i = 0; i < str.length()-1; i++) {
+            if (str.charAt(i+1)== ' ')  s += Character.toUpperCase(str.charAt(i));
+            else s += str.charAt(i);
+        }
+
+        s += Character.toUpperCase(str.charAt(s.length()));
+
+        return s;
+    }
+
+    public static int sumTwoSmallestNums(int[] arr) {
+        ArrayList<Integer> list  = new ArrayList<>();
+
+        for (int n : arr) {
+            if (n>0) list.add(n);
+        }
+        Collections.sort(list);
+
+        return list.get(0)+list.get(1);
+    }
+
+    public static int sumTwoSmallestNums2(int[] arr) {
+        int sum = 0;
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]<0)continue;
+            sum = arr[i] +arr[i+1];
+            break;
+        }
+
+        return sum;
     }
 }
