32194effdd6646521bd6c8744a79cb472eb34399
melih <nmlhky@gmail.com>
2020-04-18 00:00:52 -0400

    Commit Message

--- src/Test.java
+++ src/Test.java
@@ -2,22 +2,29 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int[] c = new  int[] {0,0,0,0,1,0};
 
-        int counter = 0;
-        int largest = 0;
+        System.out.println(jumpingOnClouds(c));
 
-        while (counter < 3) {
+    }
 
-            System.out.println("Enter the number: ");
-            int number = scanner.nextInt();
 
-            if (largest < number) {
-                largest = number;
-            }
-            counter++;
-
+    static int jumpingOnClouds(int[] c) {
+        int count = 0 ;
+        int n = c.length;
+        for (int i = 0; i < n-2 ; count++) {
+            if (c[i+2]==0  || c[i] == 1 ) i = i+2;
+            else i++;
         }
-        System.out.println("the largest number is " + largest);
+        if (c[n-2]==0) count++;
+        return count;
+
+
+
+        int count = -1;
+        for (int i = 0; i < n; i++, count++) {
+            if (i<n-2 && c[i+2]==0) i++;
+        }
+        System.out.println(count);
     }
 }
