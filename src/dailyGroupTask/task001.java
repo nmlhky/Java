82c18a4da59019c607775a0c5644deb9399db10d
melih <nmlhky@gmail.com>
2020-04-19 22:13:05 -0400

    Commit Message

--- src/dailyGroupTask/task001.java
+++ src/dailyGroupTask/task001.java
@@ -1,10 +1,84 @@
 package dailyGroupTask;
 
+import javax.swing.*;
 import java.util.Scanner;
+import java.util.SplittableRandom;
 
 public class task001 {
     public static void main(String[] args) {
-        printBFI(bodyFitIndex());
+        //printBFI(bodyFitIndex());
+
+        //System.out.println(isEqual(4,4));
+
+        System.out.println(nameShuffle("melih kaya"));
+    }
+
+    public static String getWord(String left, String right) {
+        String result = "";
+        result += left.charAt(0);
+
+        result = result.toUpperCase();
+
+        for (int i = 1; i < left.length(); i++) {
+            result += left.charAt(i);
+        }
+        for (int i = 0; i < right.length(); i++) {
+            result += right.charAt(i);
+        }
+        return result;
+    }
+
+    public static int differenceMaxMin(int[] arr) {
+        int min = arr[0] ;
+        int max = arr[0] ;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) min = arr[i];
+            if (arr[i] > max) max = arr[i];
+        }
+
+        return max-min;
+    }
+
+    public static boolean isEqual(int num1, int num2) {
+        if(num1 == num2) return true;
+        else return false;
+    }
+
+    public static int convert(int hours, int minutes) {
+        int second = (hours * 3600) + (minutes * 60);
+
+        return second;
+    }
+
+    public static String reverse(final String str) {
+        String reverse = "";
+
+        for (int i = str.length()-1 ; i >= 0  ; i--) {
+            reverse += str.charAt(i);
+        }
+        return reverse;
+    }
+
+    public static String nameShuffle(String s) {
+        String s1 = "";
+        String s2 = "";
+
+        boolean isFirstString = true;
+        for (int i = 0; i < s.length(); i++) {
+            while (isFirstString){
+                if (s.charAt(i) == ' '){
+                    isFirstString = false;
+                    i++;
+                }
+                s1 += s.charAt(i);
+                i++;
+            }
+            s2 += s.charAt(i);
+        }
+
+        String s3 = s2 + " " + s1;
+        return s3;
     }
 
     public static double bodyFitIndex(){
@@ -15,7 +89,6 @@ public class task001 {
         double height = scanner.nextDouble();
 
         double bfi = weight / Math.pow( (height/100) , 2 );
-
         return bfi;
     }
 
