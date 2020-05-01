581d735a0a42e73cf5219fbd75c5e2db45f2533e
melih <nmlhky@gmail.com>
2020-04-30 23:30:24 -0400

    .

--- src/test2.java
+++ src/test2.java
@@ -4,15 +4,6 @@ import java.util.*;
 public class test2 {
     public static void main(String[] args) {
 
-        TreeMap<String, Integer> tree2 = new TreeMap<>();
-        tree2.put("xsd",4153);
-        tree2.put("asf",4153);
-        tree2.putAll(tree2);
-        System.out.println(tree2);
-
-        List<Integer> a = new ArrayList<>(List.of(1,2,3));
-        a.addAll(a);
-        System.out.println(a);
 
 
     }
