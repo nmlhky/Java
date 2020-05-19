67f01a0a88dad2fc90d74f1d05b74fc7c14aba61
melih <nmlhky@gmail.com>
2020-05-18 22:07:31 -0400

    .

--- src/Selenium/Connect.java
+++ src/Selenium/Connect.java
@@ -4,11 +4,14 @@ package Selenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class Connect {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+        System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe");
 
         ChromeDriver driver  = new ChromeDriver();
+
         driver.get("https://www.google.com/");
         System.out.println(driver.getTitle());
         driver.findElement(By.partialLinkText("")).click();
