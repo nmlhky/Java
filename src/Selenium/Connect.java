e2f68c7ff0b48e8ca4b0cfbf3fc780edc42743a9
melih <nmlhky@gmail.com>
2020-05-20 22:59:30 -0400

    .

--- src/Selenium/Connect.java
+++ src/Selenium/Connect.java
@@ -3,17 +3,28 @@ package Selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverInfo;
+
+import java.sql.Driver;
 
 
 public class Connect {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
         System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe");
+        System.setProperty("webdriver.edge.driver","C:\\edgedriver.exe");
 
-        ChromeDriver driver  = new ChromeDriver();
+//        ChromeDriver driver  = new ChromeDriver();
+//        driver.get("https://www.google.com/");
+//        System.out.println(driver.getTitle());
 
-        driver.get("https://www.google.com/");
-        System.out.println(driver.getTitle());
-        driver.findElement(By.partialLinkText("")).click();
+//        EdgeDriver edge = new EdgeDriver();
+//        edge.get("https://www.google.com/");
+
+        FirefoxDriver d= new FirefoxDriver();
+        d.get("https://www.google.com/");
     }
+
 }
