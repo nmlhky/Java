2af02ad1d18550963564a251f59e1766fef568fe
melih <nmlhky@gmail.com>
2020-05-30 13:43:16 -0400

    .

--- src/questionsAnswers/dailyGroupTask/Task013.java
+++ src/questionsAnswers/dailyGroupTask/Task013.java
@@ -4,13 +4,13 @@ import java.util.Arrays;
 
 public class Task013 {
     public static void main(String[] args) {
-        int[][] arr = squarePatch(5);
+        int[][] arr = squarePatch(0);
         for (int[] ar : arr) {
             System.out.println(Arrays.toString(ar));
         }
-        System.out.println(Arrays.toString(squarePatch(6)));
     }
     public static int[][] squarePatch(int n) {
+
         int[][] arr = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
