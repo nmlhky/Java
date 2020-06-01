db24970a33057419e83725248eadaadf853ab519
melih <nmlhky@gmail.com>
2020-05-31 21:53:20 -0400

    .

--- src/questionsAnswers/Test.java
+++ src/questionsAnswers/Test.java
@@ -1,11 +1,32 @@
 package questionsAnswers;
 
+import java.math.BigInteger;
 import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        int[] arr = {1, 3, 3, 4};
-        System.out.println(isShorted(arr));
+        // put your code here
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger m = sc.nextBigInteger();
+        BigInteger factorial = BigInteger.ONE;
+
+        long i = 1;
+
+        while (m.compareTo(factorial) > 0 ) {
+            i++;
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+
+        System.out.println(i);
+    }
+
+    static Long factorial(Long n)
+    {
+        if (n == 0)
+            return 1L;
+
+        return n*factorial(n-1);
     }
 
     public static boolean isShorted(int[] arr){
