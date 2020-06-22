717d3cde79d6165ada5b8f8e4b8e8dcf62f47b8d
melih <nmlhky@gmail.com>
2020-06-22 15:55:30 -0400

    .

--- src/questionsAnswers/Oce.java
+++ src/questionsAnswers/Oce.java
@@ -2,6 +2,24 @@ package questionsAnswers;
 
 public class Oce {
     public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+        }while (i++ < 5);
+
+        i = 0;
+        while (++i <5){
+            System.out.println(i);
+
+        }
+
+        System.out.println();
+
+        String[][] arrrrr = new String[5][2];
+        System.out.println(arrrrr[1].length);
+        //tanesi 2 lik,  5 tane array tutuyor
+
+        //new booleen false
         boolean bo = new Boolean(null);
         System.out.println(bo);
     }
--- src/questionsAnswers/Test.java
+++ src/questionsAnswers/Test.java
@@ -3,7 +3,46 @@ package questionsAnswers;
 
 public class Test {
     public static void main(String[] args) {
+//        char[] arr = {'a' , 'b'  ,  '4' , ' ' , '3'};
+//        String[] array = {"asda","asdad","asdd","asd"};
+//
+//        int[][] arrayint = {{1,2,3},{4,5,6},{7,8,9}};
+//
+//        for (int[] arrr : arrayint) {
+//            for (int n : arrr) {
+//                System.out.print(n + " ");
+//            }
+//            System.out.println();
+//        }
+//
+//        for (int i = 0; i < arrayint.length; i++) {
+//            for (int j = 0; j < arrayint[i].length; j++) {
+//                System.out.print(i+" "+arrayint[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
+//
+//        for (String str : array) {
+//            System.out.println(str);
+//        }
+//
+//        for (char c : arr) {
+//            System.out.println(c);
+//        }
+
+        int  n = 49;
+
+        if (n == 25)
+        System.out.println("21 22 23 24 25");
+        System.out.println("20  7  8  9 10");
+        System.out.println("19  6  1  2 11");
+        System.out.println("18  5  4  3 12");
+        System.out.println("17 16 15 14 13");
+        
+
 
     }
+
+
 
 }
