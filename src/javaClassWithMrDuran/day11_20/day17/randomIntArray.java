5484f59a46c34ce969f4debf637eeca1ba5ce242
melih <nmlhky@gmail.com>
2020-09-26 02:02:01 -0400

    .

--- src/javaClassWithMrDuran/day11_20/day17/randomIntArray.java
+++ src/javaClassWithMrDuran/day11_20/day17/randomIntArray.java
@@ -2,13 +2,13 @@ package javaClassWithMrDuran.day11_20.day17;
 
 public class randomIntArray {
     public static void main(String[] args) {
-        RandomArray(5);
+        RandomArray(50);
     }
 
     public static void RandomArray(int n) {
         int[] random = new int[n];
         for (int i = 0; i < n; i++) {
-            random[i] = (int) (Math.random() * 4001) + 1000;
+            random[i] = (int) (Math.random() * 2) + 1;
         }
 
         for (int j = 0; j < n; j++) {
--- src/questionsAnswers/OCA_notlar.java
+++ src/questionsAnswers/OCA_notlar.java
@@ -3,9 +3,9 @@ package questionsAnswers;
 public class OCA_notlar {
     public static void main(String[] args) {
         //yanlislarim
-        //20,24,25,31,42
+        //20,24,25,31,42,69,77,84,93,136
         //look again
-        //46 , 48, 50 56 57
+        //46 48 50 56 57 100 101 130
 
         //new booleen false
         boolean bo = new Boolean(null);
@@ -24,7 +24,36 @@ public class OCA_notlar {
         System.out.println( ( x++ < ++y ) ? ("x = "+ x + " | y = "+ y) : "false");
 
 
+        //ans`ler hep farkli ans.
+        try {
+            int num = 10;
+            int div = 1;
+            int ans = num/ div;
+        } catch (ArithmeticException ae){
+            int ans = 0;
+        }
+        //System.out.println(ans);
+
+        //importda package degil class import yapilir
+
+
         //
+        int numbers [];
+        numbers = new int[2];
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers = new int[4];
+        numbers[2] = 30;
+        numbers[3] = 40;
+        for (int n : numbers) {
+            System.out.println(n);// 0 0 30 40
+        }
+
+        // animal boncuk = new cat() >> is cat or animal?
+
+
+
+
 
 
 
