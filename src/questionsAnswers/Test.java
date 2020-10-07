73aac4dba80634a1245e599864495e25107bf3a5
melih <nmlhky@gmail.com>
2020-10-06 21:33:07 -0400

    .

--- src/questionsAnswers/Test.java
+++ src/questionsAnswers/Test.java
@@ -1,19 +1,31 @@
 package questionsAnswers;
 
-
 import java.util.*;
-
 
 public class Test {
 
     public static void main(String[] args) {
 
-
+        int[] arr = {7,7,42,42,1,56,55,75};
+        System.out.println(solution(7,7,arr));
 
     }
 
-
-
+    public static int  solution(int X, int Y, int[] A) {
+        int N = A.length;
+        int result = -1;
+        int nX = 0;
+        int nY = 0;
+        for (int i = 0; i < N; i++) {
+            if (A[i] == X)
+                nX++;
+            else if (A[i] == Y)
+                nY++;
+            if (nX == nY)
+                result = i;
+        }
+        return result;
+    }
 
 
 }
