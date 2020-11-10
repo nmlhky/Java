33e92f480d95181c678d793513910a48d6bf8d5a
Melih <nmlhky@gmail.com>
2020-11-10 16:26:55 -0500

    .

--- src/questionsAnswers/Test.java
+++ src/questionsAnswers/Test.java
@@ -1,19 +1,25 @@
 package questionsAnswers;
 
-import java.text.ChoiceFormat;
-import java.text.Format;
-import java.text.MessageFormat;
-import java.text.NumberFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
-import java.util.function.Predicate;
-import java.util.stream.Collector;
-import java.util.stream.Stream;
 
 public class Test {
 
     public static void main(String[] args) {
+        System.out.println(getsum(5,9,6));
+    }
+
+    static long getsum(int i, int j, int k) {
+
+        long sum = 0;
+
+        for (; i <= j; i++)
+            sum +=i;
+
+
+        for (j--; j >= k; j--)
+            sum += j;
+
+
+        return sum;
 
     }
 
