c6e5573f388cd8cc60f37e641bad44ed5d9ac9d8
Melih <nmlhky@gmail.com>
2020-11-09 20:53:35 -0500

    .

--- src/questionsAnswers/Test.java
+++ src/questionsAnswers/Test.java
@@ -1,31 +1,21 @@
 package questionsAnswers;
 
+import java.text.ChoiceFormat;
+import java.text.Format;
+import java.text.MessageFormat;
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
 
 public class Test {
 
     public static void main(String[] args) {
 
-        int[] arr = {7,7,8,8,1,56,55,75,7,9};
-        System.out.println(solution(7,8,arr));
-
     }
-
-    public static int  solution(int X, int Y, int[] A) {
-        int N = A.length;
-        int result = -1;
-        int nX = 0;
-        int nY = 0;
-        for (int i = 0; i < N; i++) {
-            if (A[i] == X)
-                nX++;
-            else if (A[i] == Y)
-                nY++;
-            if (nX == nY)
-                result = i;
-        }
-        return result;
-    }
-
 
 }
+
